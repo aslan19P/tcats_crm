dfa4baccefcb92c7886301ec28cbcf8a2b0e606f
aslan <matackubovaslan@gmail.com>
2024-12-21 23:39:50 +0500

    loz

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -366,7 +366,7 @@ export default function Home() {
       html5QrCode
         .start(
           selectedCamera, // Используем выбранную камеру
-          { fps: 15, qrbox: { width: 300, height: 300 } }, // Настройки сканера
+          { fps: 15 }, // Настройки сканера
           qrCodeSuccess,
           qrCodeError
         )
